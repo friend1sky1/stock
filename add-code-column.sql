b354d269ac3959b3ca81dd0ab7204b78be1b2c6b
friend1sky1 <124789552+friend1sky1@users.noreply.github.com>
2026-08-19 14:15:56 +0800

    fix: 修复维远股份股票代码

--- add-code-column.sql
+++ add-code-column.sql
@@ -3,5 +3,5 @@ ALTER TABLE stocks ADD COLUMN IF NOT EXISTS code TEXT DEFAULT '';
 
 -- 为现有股票设置代码
 UPDATE stocks SET code = 'sh600316' WHERE name = '洪都航空';
-UPDATE stocks SET code = 'sz002461' WHERE name = '维远股份';
+UPDATE stocks SET code = 'sh600955' WHERE name = '维远股份';
 UPDATE stocks SET code = 'sz000725' WHERE name = '京东方A';
